8fc1a43396783b5b5d60589fcdc662a1015f678e
okok111 <1for.rails@gmail.com>
2023-07-31 08:27:14 +0900

    fullcalendarとは関係ない機能を置いておくブランチ（タグ、SNS投稿）

---
+++ app/javascript/calendar.js
@@ -0,0 +1,33 @@
+// app/javascript/calendar.js (example file name)
+import { Calendar } from '@fullcalendar/core';
+import interactionPlugin from '@fullcalendar/interaction';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+
+document.addEventListener('turbolinks:load', function() {
+  var calendarEl = document.getElementById('calendar');
+
+  var calendar = new Calendar(calendarEl, {
+    plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
+    locale: 'ja',
+    timeZone: 'Asia/Tokyo',
+    headerToolbar: {
+      start: 'prev,next,today',
+      center: 'title',
+      end: 'dayGridMonth,timeGridWeek,timeGridDay'
+    },
+    buttonText: {
+      today: '今日'
+    },
+    events:'/posts.json',
+
+    // Handle the event click
+    eventClick: function(arg) {
+      var eventUrl = '/posts/' + arg.event.id; // Assuming your event has an 'id' attribute
+      window.location.href = eventUrl;
+      info.el.style.borderColor = 'red';
+    },
+  });
+
+  calendar.render();
+});
